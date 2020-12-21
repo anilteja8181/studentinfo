96988dd8ed01888faf81126ba83d14c1ec3dc49f
LENOVO <LENOVO@LAPTOP-1GEF5DG6>
2020-12-21 14:14:07 +0530

    Added test

--- studentinfo/src/test/java/com/example/studentinfo/StudentinfoApplicationTests.java
+++ studentinfo/src/test/java/com/example/studentinfo/StudentinfoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.studentinfo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,9 @@ class StudentinfoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals("greeting", "greeting");
+		
+		
 	}
 
 }
